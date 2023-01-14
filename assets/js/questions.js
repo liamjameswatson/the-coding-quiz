b93b962dcf7bb966fbf1d3b9bcdcbabd5f0db032
Liam <liamjwatson@aol.com>
2023-01-14 03:05:35 +0000

    question title and answers displayed to screen

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,15 +11,29 @@ var questionArray = [
   }),
 ];
 
+var questionsDiv = document.querySelector("#questions");
+
+var questionUl = document.createElement("ul");
+var questionLi = document.createElement("li");
+
+questionUl.appendChild(questionLi);
+questionLi.innerHTML = "heellloooo";
+
 function displayQuestionsAndAnswers(questionArray) {
   for (var i = 0; i < questionArray.length; i++) {
+    questionsDiv.children[0].innerHTML = questionArray[i].questionText;
     console.log(questionArray[i].questionText);
+    questionsDiv.innerHTML = questionArray[i].questionText;
+    var questionUl = document.createElement("ul");
+    questionsDiv.appendChild(questionUl);
+
     console.log("------------------------------------");
     for (var j = 0; j < questionArray[i].answers.length; j++) {
       console.log(questionArray[i].answers[j]);
+      var questionLi = document.createElement("li");
+      questionUl.appendChild(questionLi);
+      questionLi.innerHTML = questionArray[i].answers[j];
     }
     console.log("#####################################");
   }
 }
-
-displayQuestionsAndAnswers(questionArray);
