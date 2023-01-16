08ad4e828b6a68546b43b9a19126cd5d8b39f597
Liam <liamjwatson@aol.com>
2023-01-16 19:49:18 +0000

    split functions into two - one for displayQuestion - one for validateQuestion

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -28,47 +28,44 @@ function hideStartScreen() {
   return startScreen.classList.add("hide");
 }
 
-var answerListItem;
-// show the question div
-// ----------------------------------------DISPLAY
+// -----------DISPLAY Question -------------------------
 function displayQuestion(currentQuestion) {
   // Remove the hide class to show div
   questionDiv.classList.remove("hide");
-
+  // questionDiv.innerHTML = "";
+  var question = quizArray[currentQuestion];
+  var answerList = document.createElement("ul");
   // current question displayed in questionDiv h2
-  for (var i = 0; i < quizArray.length; i++) {
-    var answerList = document.createElement("ul");
-    questionDiv.children[0].innerHTML = quizArray[currentQuestion].questionText;
-  }
+  questionDiv.children[0].innerHTML = question.questionText;
   // In choices div - make a unordered list
   questionDiv.children[1].appendChild(answerList);
+
   // for each answer of the current question
   for (var i = 0; i < quizArray[currentQuestion].answers.length; i++) {
     // Create a list-item and display the answer
-    answerListItem = document.createElement("li");
+    var answerListItem = document.createElement("li");
     answerList.appendChild(answerListItem).innerHTML =
       quizArray[currentQuestion].answers[i];
-    answerListItem.addEventListener("click", function (event) {
-      // if user clicks on right answer
-      if (event.target.innerHTML === quizArray[currentQuestion].correctAnswer) {
-        console.log("correct");
-        // hide the current Question
-        questionDiv.classList.add("hide");
-        // increase the current question + 1
-        currentQuestion++;
-        console.log("currentQuestion = " + currentQuestion);
-      } else {
-        // if user clicks on wrong answer
-        console.log("wrong");
-        // hide the current Question
-        questionDiv.classList.add("hide");
-        // increase the current question + 1
-        currentQuestion++;
-        // increase the timer by 10 seconds
-        timerCount = +10;
-        console.log("currentQuestion = " + currentQuestion);
-      }
-    });
+  }
+}
+
+// ---------------Check Answer-------------------
+function checkAnswer(event) {
+  // if user clicks on right answer
+  if (event.target.innerHTML === quizArray[currentQuestion].correctAnswer) {
+    console.log("correct");
+    // increase the current question + 1
+    currentQuestion++;
+    console.log("currentQuestion = " + currentQuestion);
+  } else {
+    // if user clicks on wrong answer
+    console.log("wrong");
+
+    // increase the current question + 1
+    currentQuestion++;
+    // increase the timer by 10 seconds
+    timerCount = +10;
+    console.log("currentQuestion = " + currentQuestion);
   }
 }
 
@@ -78,4 +75,17 @@ startButton.addEventListener("click", function () {
   questionDiv.className = "show";
   currentQuestion = 0;
   displayQuestion(currentQuestion);
+  choices.addEventListener("click", checkAnswer);
 });
+
+// if (currentQuestion < quizArray.length) {
+//   displayQuestion(currentQuestion);
+//   choices.addEventListener("click", checkAnswer);
+// }
+
+// for (var i = 0; i < quizArray.length; i++) {
+//   displayQuestion(currentQuestion);
+//   choices.addEventListener("click", checkAnswer);
+// }
+
+// questionDiv.innerHTML = "";
