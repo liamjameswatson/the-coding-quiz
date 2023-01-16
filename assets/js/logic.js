aa8bfecf3e4d2a4222694e702a00b785abeec5a8
Liam <liamjwatson@aol.com>
2023-01-16 15:07:16 +0000

    answer validation console.logged, and timer inreases when wrong

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,28 +1,10 @@
 var startButton = document.querySelector("#start");
 var timer = document.querySelector("#time");
 var startScreen = document.querySelector("#start-screen");
-var questionsDiv = document.querySelector("#questions");
+var questionDiv = document.querySelector("#questions");
+var choices = document.querySelector("#choices");
+var endScreen = document.querySelector("#end-screen");
 // ----------------------------------------------------------
-
-
-
-// A start button that when clicked a timer starts and the first question appears.
-
-// - Questions contain buttons for each answer.
-
-// - When answer is clicked, the next question appears
-
-// - If the answer clicked was incorrect then subtract time from the clock
-
-// - The quiz should end when all questions are answered or the timer reaches 0.
-
-// - When the game ends, it should display their score and give the user the ability to save their initials and their score
-
-// A start button that when clicked a timer starts and the first question appears.
-
-var displayAnswers = displayQuestionsAndAnswers(questionArray);
-
-// startButton.addEventListener("cli);
 
 // Make a var playQuiz = false
 var playQuiz = false;
@@ -33,57 +15,67 @@ var Currentscore;
 // Make a var highScore = 0
 var highScore;
 
-// click listen event on button - when clicked - set playQuiz = true
-
-// while playGame = true (){
-
-// <!-- Game starts here -->
-
-// timerCount = 60seconds - countsDown
-
-//   if when clicked === true
-//   current score = currentScore + 10
-//   display next
-//   else{
-//       current score = currentScore
-//       timer = timer - 10 seconds
-//   }
-
-//   if timer === 0{
-//       gameOver - display score
-//   }
-
-// If (currentScore is higher than highScore){
-// currentScore = highScore
-// }
-
-// }
-
-//   display questions - answers have click listen events on them
-
-//   if (answer clicked === false) {
-//       take 10 secs off the time
-//   }
-//   else{
-//       continue
-//   }; -->
-
-// }
-
 startTimer = function () {
   var time = setInterval(function () {
     timerCount++;
     timer.innerHTML = timerCount;
-    if (timerCount === 5) {
-      timerCount--;
-    }
   }, 1000);
   return time;
 };
 
+// hide the start
+function hideStartScreen() {
+  return startScreen.classList.add("hide");
+}
+
+var answerListItem;
+// show the question div
+// ----------------------------------------DISPLAY
+function displayQuestion(currentQuestion) {
+  // Remove the hide class to show div
+  questionDiv.classList.remove("hide");
+
+  // current question displayed in questionDiv h2
+  for (var i = 0; i < quizArray.length; i++) {
+    var answerList = document.createElement("ul");
+    questionDiv.children[0].innerHTML = quizArray[currentQuestion].questionText;
+  }
+  // In choices div - make a unordered list
+  questionDiv.children[1].appendChild(answerList);
+  // for each answer of the current question
+  for (var i = 0; i < quizArray[currentQuestion].answers.length; i++) {
+    // Create a list-item and display the answer
+    answerListItem = document.createElement("li");
+    answerList.appendChild(answerListItem).innerHTML =
+      quizArray[currentQuestion].answers[i];
+    answerListItem.addEventListener("click", function (event) {
+      // if user clicks on right answer
+      if (event.target.innerHTML === quizArray[currentQuestion].correctAnswer) {
+        console.log("correct");
+        // hide the current Question
+        questionDiv.classList.add("hide");
+        // increase the current question + 1
+        currentQuestion++;
+        console.log("currentQuestion = " + currentQuestion);
+      } else {
+        // if user clicks on wrong answer
+        console.log("wrong");
+        // hide the current Question
+        questionDiv.classList.add("hide");
+        // increase the current question + 1
+        currentQuestion++;
+        // increase the timer by 10 seconds
+        timerCount = +10;
+        console.log("currentQuestion = " + currentQuestion);
+      }
+    });
+  }
+}
+
 startButton.addEventListener("click", function () {
-  displayQuestionsAndAnswers(questionArray);
   startTimer();
   startScreen.setAttribute("hidden", "hide");
-  questionsDiv.className = "show";
+  questionDiv.className = "show";
+  currentQuestion = 0;
+  displayQuestion(currentQuestion);
 });
