5026392820c9da9684d54fcee7664660c53fd042
Liam <liamjwatson@aol.com>
2023-01-18 17:01:05 +0000

    timer ticks down from 60 secs, -10 points for wrong answer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,6 @@
+var startButton = document.querySelector("#start");
 var startScreen = document.querySelector(".start");
+var timer = document.querySelector("#time");
 var questionDiv = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var choices = document.querySelector("#choices");
@@ -8,21 +10,56 @@ var highScore = 0;
 var score = 0;
 var currentQuestion = 0;
 
+// --- set timer 60 seconds
+var timerCount = 60;
+timer.innerText = timerCount;
+startTimer = function () {
+  var time = setInterval(function () {
+    timerCount--;
+    timer.innerHTML = timerCount;
+  }, 1000);
+  return time;
+};
+
 function displayQuestion(currentQuestion) {
-  console.log("displaying question..." + currentQuestion);
   // display the question
   questionTitle.innerText = quizArray[currentQuestion].question;
   //   make a unordered list - for the choices
   choices.appendChild(choicesList);
 
   for (var i = 0; i < quizArray[currentQuestion].choices.length; i++) {
+    console.log("iiiiii" + i);
     // for each choice create a list item
     var choice = document.createElement("li");
     // display each choice on each list item
     choicesList.appendChild(choice).innerText =
       quizArray[currentQuestion].choices[i];
   }
-  checkAnswer(currentQuestion);
+}
+checkAnswer(currentQuestion);
+function checkAnswer(currentQuestion) {
+  // match the click to the answer
+  choicesList.addEventListener("click", function (event) {
+    // console.log(event.target.innerText);
+    questionTitle.innerText = "";
+    choicesList.innerText = "";
+    if (event.target.innerText === quizArray[currentQuestion].answer) {
+      score = score + 10;
+      // console.log(score);
+    } else {
+      console.log("wrong");
+      timerCount = timerCount - 10;
+    }
+    console.log("currentQuestion..." + currentQuestion);
+    console.log("quiz array..." + (quizArray.length - 1));
+    console.log("Score..." + score);
+    if (currentQuestion === quizArray.length - 1) {
+      console.log("game over");
+    } else {
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    }
+  });
 }
 
 function startQuiz() {
@@ -32,37 +69,9 @@ function startQuiz() {
   currentQuestion = 0;
 }
 
-function checkAnswer(currentQuestion) {
-  console.log("checking question..." + currentQuestion);
-  // match the click to the answer
-  choicesList.addEventListener("click", function (event) {
-    console.log(event.target.innerText);
-    questionTitle.innerText = "";
-    choicesList.innerText = "";
-    if (event.target.innerText === quizArray[currentQuestion].answer) {
-      console.log("correct " + currentQuestion);
-      //   clear inner text
-      //   currentQuestion++
-      currentQuestion++;
-      score = +10;
-      console.log(score);
-      //   display next question
-    } else {
-      // if user clicks on wrong answer
-      console.log("wrong");
-
-      // increase the current question + 1
-      currentQuestion++;
-      // increase the timer by 10 seconds
-      timerCount = +10;
-      console.log("currentQuestion = " + currentQuestion);
-    }
-    displayQuestion(currentQuestion);
-  });
-}
-
-startQuiz();
-questionDiv.classList.remove("hide");
-displayQuestion(currentQuestion);
-
-console.log(quizArray[0].choices.length);
+startButton.addEventListener("click", function () {
+  startQuiz();
+  startTimer();
+  questionDiv.classList.remove("hide");
+  displayQuestion(currentQuestion);
+});
