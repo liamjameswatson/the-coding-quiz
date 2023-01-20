08b757d3ff70445c10b72bb6be612c9e62265523
Liam <liamjwatson@aol.com>
2023-01-20 13:51:47 +0000

    10 javaScript questions added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,8 +10,45 @@ var quizArray = [
     answer: "1995",
   },
   {
-    question: "Who won the FA cup in 1976?",
-    choices: ["Southampton", "Arsenal", "Man Utd", "Leeds"],
-    answer: "Southampton",
+    question: "A javaScript file has which extension?",
+    choices: [".css", ".xml", ".html", ".js"],
+    answer: ".js",
+  },
+  {
+    question:
+      "Which of the following displays a message and allows the user to type input?",
+    choices: ["alert()", "prompt()", "confirm()", "msg()"],
+    answer: "prompt()",
+  },
+  {
+    question:
+      "I need to display something on the console in JavaScript. Which of the following do I need?",
+    choices: ["console.log", "print", "display", "show"],
+    answer: "console.log",
+  },
+  {
+    question: "Which element links a javaScript file linked to a html file?",
+    choices: ["js", "connect", "script", "link"],
+    answer: "script",
+  },
+  {
+    question: "var myNumbers = [1, 2, 3]\nmyNumbers an example of what?",
+    choices: ["boolean", "method", "array", "object"],
+    answer: "array",
+  },
+  {
+    question: "What type of data structure uses key, value pairs?",
+    choices: ["variable", "object", "array", "boolean"],
+    answer: "object",
+  },
+  {
+    question: "How do you repeat the same block of code, in multiple places, with javaScript? ?",
+    choices: ["A data type", "An array", "A function", "An element"],
+    answer: "A function",
+  },
+  {
+    question: "jquery is a type of what?",
+    choices: ["libary", "boolean", "expression", "function"],
+    answer: "library",
   },
 ];
